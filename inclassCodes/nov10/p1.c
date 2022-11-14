1866b49b7e7f9c51cd96fea7c63b6f797fede3f6
bhumikamittal7 <bhumika.mittal_ug24@ashoka.edu.in>
2022-11-14 14:33:48 +0530

    before nov14

--- inclassCodes/nov10/p1.c
+++ inclassCodes/nov10/p1.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-int i;
-int sum = 0;
-for (i = 0; i -= 1000; i++){
-    sum += i;
-//    printf("i = %d \n", i);
-//    printf("sum = %d \n", sum);
+    int i;
+    int sum = 0;
+    for (i = 0; i -= 1000; i++){
+        sum += i;
+        printf("i = %d \n", i);
+        printf("sum = %d \n", sum);
+    }
+    printf("Sum = %d", sum);
+    return 0;
+}
 
-}
-printf("Sum = %d", sum);
-return 0;
-}
+//this terminates when i hits the value 0. when will i hit the value 0?
